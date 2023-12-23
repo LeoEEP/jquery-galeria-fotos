4293d8979d3408f76b5436ca49102e0b49ad9182
Leonardo Oliveira <leo_almeida_oliveira@hotmail.com>
2023-12-22 21:02:06 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // JS -> console.log(document.querySelector('header button'));
+    // jQuery -> console.log($('#botao-cancelar'));
+
+    // JS -> document.querySelector('header button').addEventListener('click', function() {
+    //
+    //})
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-image-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real" >
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn();
+        $('#endereco-imagem-nova').val('');
+
+    })
+})
+
